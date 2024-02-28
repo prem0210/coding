487c2139b627a422a4ff8c80d65960b06a721f86
prem0210 <159036717+prem0210@users.noreply.github.com>
2024-02-28 23:01:44 +0530

    Delete MultiplyFirst100Numbers.java

--- MultiplyFirst100Numbers.java
+++
@@ -1,9 +0,0 @@
-public class MultiplyFirst100Numbers {
-    public static void main(String[] args) {
-        long product = 1;
-        for (int i = 1; i <= 100; i++) {
-            product *= i;
-        }
-        System.out.println("Product of the first 100 numbers: " + product);
-    }
-}
